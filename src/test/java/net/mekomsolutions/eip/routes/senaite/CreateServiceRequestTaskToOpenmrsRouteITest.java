c1bc4a402de6d89d3aef91d3f10d0cd0fcbfa86f
ruhanga <ruhanganathan@gmail.com>
2023-01-18 14:17:49 +0300

    OZ-111: Adding test coverage

--- src/test/java/net/mekomsolutions/eip/routes/senaite/CreateServiceRequestTaskToOpenmrsRouteITest.java
+++ src/test/java/net/mekomsolutions/eip/routes/senaite/CreateServiceRequestTaskToOpenmrsRouteITest.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.ModelCamelContext;
@@ -25,7 +26,10 @@ public class CreateServiceRequestTaskToOpenmrsRouteITest extends BaseWatcherRout
     private MockEndpoint authenticateToOpenmrs;
     
     @EndpointInject(value = "mock:createTaskOpenmrsEndpoint")
-    private MockEndpoint createTaskOpenmrsEndpoint; 
+    private MockEndpoint createTaskOpenmrsEndpoint;
+    
+    @EndpointInject(value = "mock:searchTaskBasedOnServiceRequestOpenmrsEndpoint")
+    private MockEndpoint searchTaskBasedOnServiceRequestOpenmrsEndpoint; 
     
     @Before
     public void setup() throws Exception {
@@ -36,6 +40,7 @@ public class CreateServiceRequestTaskToOpenmrsRouteITest extends BaseWatcherRout
     	    public void configure() throws Exception {
     	    	weaveByToString("To[direct:authenticate-to-openmrs]").replace().toD("mock:authenticateToOpenmrsRoute");
     	    	weaveByToString("DynamicTo[{{fhirR4.baseUrl}}/Task]").replace().toD("mock:createTaskOpenmrsEndpoint");
+    	    	weaveByToString("DynamicTo[{{fhirR4.baseUrl}}/Task?based-on:ServiceRequest=${exchangeProperty.lab-order-uuid}&throwExceptionOnFailure=false]").replace().toD("mock:searchTaskBasedOnServiceRequestOpenmrsEndpoint");
     	    }
     	});
     	
@@ -46,10 +51,11 @@ public class CreateServiceRequestTaskToOpenmrsRouteITest extends BaseWatcherRout
     @After
     public void reset() throws Exception {
     	createTaskOpenmrsEndpoint.reset();
+    	searchTaskBasedOnServiceRequestOpenmrsEndpoint.reset();
     }
 
     @Test
-    public void shouldCreateServiceRequestTaskOnOpenmrs() throws Exception {
+    public void shouldCreateServiceRequestTaskOnOpenmrsGivenTaskDoesNotExist() throws Exception {
     	// setup
     	Exchange exchange = new DefaultExchange(camelContext);
     	exchange.setProperty("lab-order-uuid", "8ee73df9-b80e-49d9-9fd1-8a5b6864178f");
@@ -60,12 +66,39 @@ public class CreateServiceRequestTaskToOpenmrsRouteITest extends BaseWatcherRout
     	// verify
     	authenticateToOpenmrs.assertExchangeReceived(0);
     	authenticateToOpenmrs.assertIsSatisfied();
+    	searchTaskBasedOnServiceRequestOpenmrsEndpoint.assertIsSatisfied();
     	createTaskOpenmrsEndpoint.assertIsSatisfied();
+    }
+    
+    @Test
+    public void shouldNotCreateServiceRequestTaskOnOpenmrsGivenTaskExists() throws Exception {
+    	// setup
+    	Exchange exchange = new DefaultExchange(camelContext);
+    	exchange.setProperty("lab-order-uuid", "order-with-corresponding-existing-FHIR-task");
+    	
+    	// replay
+    	producerTemplate.send("direct:create-serviceRequestTask-to-openmrs", exchange);
+    	
+    	// verify
+    	authenticateToOpenmrs.assertExchangeReceived(0);
+    	authenticateToOpenmrs.assertIsSatisfied();
+    	searchTaskBasedOnServiceRequestOpenmrsEndpoint.assertIsSatisfied();
+    	createTaskOpenmrsEndpoint.assertIsNotSatisfied();
     }
     
     private void setupExpectations() {
     	createTaskOpenmrsEndpoint.expectedHeaderReceived(Exchange.HTTP_METHOD, "POST");
     	createTaskOpenmrsEndpoint.expectedBodiesReceived("{\"resourceType\": \"Task\", \"status\": \"requested\", \"intent\": \"order\", \"basedOn\": [{\"reference\":\"8ee73df9-b80e-49d9-9fd1-8a5b6864178f\", \"type\": \"ServiceRequest\"}]}");
     	createTaskOpenmrsEndpoint.expectedPropertyReceived("lab-order-uuid", "8ee73df9-b80e-49d9-9fd1-8a5b6864178f");
+    	searchTaskBasedOnServiceRequestOpenmrsEndpoint.whenAnyExchangeReceived(new Processor () {
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				if ("8ee73df9-b80e-49d9-9fd1-8a5b6864178f".equals(exchange.getProperty("lab-order-uuid"))) {
+					exchange.getIn().setBody("{\"total\":0,\"rest-of-the-FHIR-bundle-payload\": {}}");
+				} else {
+					exchange.getIn().setBody("{\"total\":1,\"rest-of-the-FHIR-bundle-payload\": {}}");
+				} 
+			}		
+    	});
     }
 }
