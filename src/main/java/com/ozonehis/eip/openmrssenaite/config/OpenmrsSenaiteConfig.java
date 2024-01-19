a4ec3aefb5d28f8dc4f0279e302c15fdace48809
corneliouzbett <kipchubett@gmail.com>
2024-01-19 14:37:05 +0300

    Re-introduce the named-space properties file

---
+++ src/main/java/com/ozonehis/eip/openmrssenaite/config/OpenmrsSenaiteConfig.java
@@ -0,0 +1,8 @@
+package com.ozonehis.eip.openmrssenaite.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("eip-openmrs-senaite.properties")
+public class OpenmrsSenaiteConfig {}
