a6a647d1264bb04585cb6908ac13b3c67f909768
Siddharth Vaish <sidvaish97@gmail.com>
2024-06-19 22:15:17 +0530

    OZ-624: Add senaite model

---
+++ src/main/java/com/ozonehis/eip/openmrs/senaite/mapper/ToSenaiteMapping.java
@@ -0,0 +1,18 @@
+package com.ozonehis.eip.openmrs.senaite.mapper;
+
+/**
+ * An Interface for mapping from FHIR Resources to SENAITE resource
+ *
+ * @param <F> FHIR Resource
+ * @param <E> SENAITE resource
+ */
+public interface ToSenaiteMapping<F, E extends SenaiteResource> {
+
+    /**
+     * Maps a FHIR Resource to an SENAITE resource
+     *
+     * @param fhirResource FHIR Resource
+     * @return SENAITE resource
+     */
+    E toSenaite(F fhirResource);
+}
---
+++ src/main/java/com/ozonehis/eip/openmrs/senaite/model/Client.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.eip.openmrs.senaite.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ozonehis.eip.openmrs.senaite.mapper.SenaiteResource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Client implements SenaiteResource {
+
+//    <simple>{"portal_type":"Client","title":"${exchangeProperty.patient-name-unique}",
+//    "ClientID":"${exchangeProperty.patient-id}","parent_path":"/senaite/clients"}</simple>
+
+    @JsonProperty("portal_type")
+    private String portalType; // Client
+
+    @JsonProperty("title")
+    private String title; // patient-name-unique
+
+    @JsonProperty("ClientID")
+    private String clientID; // patient-id
+
+    @JsonProperty("parent_path")
+    private String parentPath; // /senaite/clients
+}
