1125fc6db4553c8ddc353435b8f2a946e7b37899
Siddharth Vaish <sidvaish97@gmail.com>
2024-08-09 20:30:00 +0530

    OZ-566: Use search component

--- senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/openmrsFhirTask/GetOpenmrsFhirTaskByStatusRoute.java
+++ senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/openmrsFhirTask/GetOpenmrsFhirTaskByStatusRoute.java
@@ -7,18 +7,12 @@
  */
 package com.ozonehis.eip.openmrs.senaite.routes.openmrsFhirTask;
 
-import com.ozonehis.eip.openmrs.senaite.Constants;
-import com.ozonehis.eip.openmrs.senaite.client.OpenmrsFhirClient;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GetOpenmrsFhirTaskByStatusRoute extends RouteBuilder {
-
-    @Autowired
-    private OpenmrsFhirClient openmrsFhirClient;
 
     public static final String GET_BY_STATUS_ENDPOINT = "/Task?status=requested,accepted";
 
@@ -28,10 +22,10 @@ public class GetOpenmrsFhirTaskByStatusRoute extends RouteBuilder {
         from("direct:openmrs-get-task-by-status-route")
                 .log(LoggingLevel.INFO, "Fetching Task by Status in OpenMRS...")
                 .routeId("openmrs-get-task-by-status-route")
-                .setHeader(Constants.CAMEL_HTTP_METHOD, constant(Constants.GET))
-                .setHeader(Constants.CONTENT_TYPE, constant(Constants.APPLICATION_JSON))
-                .setHeader(Constants.AUTHORIZATION, constant(openmrsFhirClient.authHeader()))
-                .toD(openmrsFhirClient.getOpenmrsFhirBaseUrl() + GET_BY_STATUS_ENDPOINT)
+                .toD("fhir://search/searchByUrl?url=" + GET_BY_STATUS_ENDPOINT)
+                .marshal()
+                .fhirJson("R4")
+                .convertBodyTo(String.class)
                 .end();
         // spotless:on
     }
--- senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/openmrsFhirTask/GetOpenmrsFhirTaskRoute.java
+++ senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/openmrsFhirTask/GetOpenmrsFhirTaskRoute.java
@@ -8,17 +8,12 @@
 package com.ozonehis.eip.openmrs.senaite.routes.openmrsFhirTask;
 
 import com.ozonehis.eip.openmrs.senaite.Constants;
-import com.ozonehis.eip.openmrs.senaite.client.OpenmrsFhirClient;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GetOpenmrsFhirTaskRoute extends RouteBuilder {
-
-    @Autowired
-    private OpenmrsFhirClient openmrsFhirClient;
 
     public static final String GET_ENDPOINT = "/Task?based-on:ServiceRequest=";
 
@@ -26,13 +21,13 @@ public class GetOpenmrsFhirTaskRoute extends RouteBuilder {
     public void configure() {
         // spotless:off
         from("direct:openmrs-get-task-route")
-                .log(LoggingLevel.INFO, "Fetching Task in OpenMRS...")
-                .routeId("openmrs-get-task-route")
-                .setHeader(Constants.CAMEL_HTTP_METHOD, constant(Constants.GET))
-                .setHeader(Constants.CONTENT_TYPE, constant(Constants.APPLICATION_JSON))
-                .setHeader(Constants.AUTHORIZATION, constant(openmrsFhirClient.authHeader()))
-                .toD(openmrsFhirClient.getOpenmrsFhirBaseUrl() + GET_ENDPOINT + "${header."
+            .log(LoggingLevel.INFO, "Fetching Task in OpenMRS...")
+            .routeId("openmrs-get-task-route")
+            .toD("fhir://search/searchByUrl?url=" + GET_ENDPOINT + "${header."
                         + Constants.HEADER_SERVICE_REQUEST_ID + "}")
+            .marshal()
+            .fhirJson("R4")
+            .convertBodyTo(String.class)
                 .end();
         // spotless:on
     }
