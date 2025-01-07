cec2a0204a90ebead51bfcb763953e0218b6965a
ruhanga <ruhanganathan@gmail.com>
2025-01-07 17:41:58 +0300

    Debugging

--- senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/handlers/openmrs/ObservationHandler.java
+++ senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/handlers/openmrs/ObservationHandler.java
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.api.Resource;
 
 @Slf4j
 @Setter
