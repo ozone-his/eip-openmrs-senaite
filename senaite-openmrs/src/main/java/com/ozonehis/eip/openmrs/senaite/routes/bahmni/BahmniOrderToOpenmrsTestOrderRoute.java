777980c8604652a92b0a93d98e8a4137f1983647
ruhanga <ruhanganathan@gmail.com>
2025-01-27 21:06:00 +0300

    Added auto creation of tests

--- senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/bahmni/BahmniOrderToOpenmrsTestOrderRoute.java
+++ senaite-openmrs/src/main/java/com/ozonehis/eip/openmrs/senaite/routes/bahmni/BahmniOrderToOpenmrsTestOrderRoute.java
@@ -14,6 +14,7 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.sql.SqlComponent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
